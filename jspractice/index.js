307b9cc27c23696ae73cf5f33af57ca2cbfc5ab9
taikurachi <tk2958@nyu.edu>
2023-04-11 10:22:52 -0400

    new

--- jspractice/index.js
+++ jspractice/index.js
@@ -1,4 +1,22 @@
-let number = 5;
-console.log(number);
+const button = document.querySelector("#myButton");
 
-Math.round(0.9);
+button.addEventListener("click", function () {
+  button.textContent = "Thanks for clicking!";
+  button.style.backgroundColor = "red";
+  button.style.textAlign = "center";
+  button.style.margin = "auto";
+});
+
+button.addEventListener("mouseenter", function () {
+  button.style.color = "white";
+  button.style.backgroundColor = "blue";
+  button.style.border = "3px solid yellow";
+});
+
+button.addEventListener("mouseleave", function () {
+  button.style.color = "black";
+  button.style.backgroundColor = "white";
+  button.style.border = "none";
+  button.style.textAlign = "left";
+  button.style.margin = "0";
+});
